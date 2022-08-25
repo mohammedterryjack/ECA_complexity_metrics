f4d19426a0cc2aba51d37a380bc8e9b94eb59dc6
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-08-25 19:35:45 +0100

    Add files via upload

--- measure_data.py
+++ measure_data.py
@@ -3,6 +3,7 @@ from glob import glob
 from json import dump
 from numpy import load 
 from metrics.lossless_fourier_compression_metric import LosslessFourierCompression
+from metrics.run_length_encoding import RunLengthEncoding
 
 def measure_dataset(data_path:str, complexity_metrics:Dict[str,callable]) -> None:
     data_results = dict()
@@ -20,17 +21,20 @@ def measure_dataset(data_path:str, complexity_metrics:Dict[str,callable]) -> Non
     with open(f"{data_path}/results.json","w") as results_file:
         dump(data_results, results_file, indent = 3)
 
-measure_dataset(
-    data_path="data",
-    complexity_metrics = dict(
-        foo =lambda s:0.0,
-        bar =lambda s:1.0,
-    )
-)
+#measure_dataset(
+#   data_path="data",
+#   complexity_metrics = dict(
+       #LosslessFourierCompression =lambda s:LosslessFourierCompression(spacetime_evolution=s).complexity,
+#       RunLengthEncoding=lambda s:1/RunLengthEncoding.compression_ratio(image=s),
+#   )
+#)
 
+#LZW, BZIP2, Huffman encoding,  Arithmetic encoding
+#Entropy encoding, BDM, Block Shannon Entropy
+with open("data/rule2_ic8932_reflection.npy", 'rb') as spacetime_file:
+    s = load(spacetime_file)
+    #print(1/RunLengthEncoding.CR(s))
 
-#results = LosslessFourierCompression(spacetime_evolution=s, verbose=True)
-#print(results.complexity) 
 
 #SYMMETRY
 #    name = filename.lstrip(f"{path}/")
