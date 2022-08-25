76e2ae9cab25c0acd5cf46d0b009f0b428c28841
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-08-25 11:26:42 +0100

    Add files via upload

---
+++ metrics/lossless_fourier_compression_metric/utils.py
@@ -0,0 +1,20 @@
+from typing import List
+
+R = List[List[float]]
+S = List[List[int]]
+Z = List[List[complex]]
+class 𝝋:
+    def __init__(self, mask:S) -> None:
+        self.m = mask
+    
+    def __hash__(self) -> str:
+        return hash(str(self))
+
+    def __eq__(self, other:"𝝋") -> bool:
+        return str(self) == str(other)
+    
+    def __repr__(self) -> str:
+        return str(self.m)
+    
+    def __call__(self, z:Z) -> Z:
+        return z*self.m
