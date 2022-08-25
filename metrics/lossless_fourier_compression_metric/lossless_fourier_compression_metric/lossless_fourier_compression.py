79e67a65de8d8e1b2c78e1a89e0b3d104e9b2069
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-08-25 19:36:19 +0100

    Add files via upload

--- metrics/lossless_fourier_compression_metric/lossless_fourier_compression_metric/lossless_fourier_compression.py
+++ metrics/lossless_fourier_compression_metric/lossless_fourier_compression_metric/lossless_fourier_compression.py
@@ -1,9 +1,7 @@
-from typing import Optional, Generator, Tuple, List
-from copy import deepcopy
+from typing import Optional, List
 
 from scipy.fft import fft2, ifft2
-from numpy import array, count_nonzero, argwhere, zeros
-from sympy.utilities.iterables import multiset_permutations
+from numpy import array, count_nonzero, flatnonzero, zeros
 
 
 from metrics.lossless_fourier_compression_metric.utils import S,R,C,𝝋
@@ -14,26 +12,22 @@ class LosslessFourierCompression:
         spacetime_evolution:S,
         quantisation_threshold:float=0.5,
         optimal_lossless_filter:Optional[𝝋]=None,
-        beam_width:int=1,
-        expansion:int=1,
         verbose:bool=False
     ) -> None:
         self.θ = quantisation_threshold
-        self.𝝋_star = self.findNearOptimalFilterFast(
+        self.𝝋_star = self.𝝋_prime_search( 
             s=spacetime_evolution,
-            beam_width=beam_width,
-            expansion=expansion,
-            verbose=verbose
         ) if optimal_lossless_filter is None else optimal_lossless_filter
+        if verbose:
+            self.display(
+                s=self.s,
+                z=self.z,
+                z_hat=self.z_hat,
+                r_hat=self.r_hat,
+                s_hat=self.s_hat,
+                losses=self.losses
+            )
         self.complexity = self.K(spacetime_evolution)
-
-    def draw(self) -> None:
-        #plot(losses)
-        #xlabel("Compression Length l(z_hat)")
-        #ylabel("Loss ɛ(s,s_hat)")
-        #show()
-        #TODO: self.s, self.z, self.z_hat, self.r_hat, self.s_hat
-        raise NotImplementedError
 
     def C(self,s:S) -> C:
         """lossless compression"""
@@ -53,48 +47,29 @@ class LosslessFourierCompression:
         assert not self.ɛ(self.s,self.s_hat), "fourier filter is not lossless!"
         return 1/self.CR(self.z,self.z_hat)
 
-    def findNearOptimalFilterFast(self, s:S, beam_width:int,expansion:int,verbose:bool) -> 𝝋:
-        """biased search for a good lossless filter"""
+    def 𝝋_prime_search(self, s:S) -> 𝝋:
+        """
+        search for near-optimal lossless filter
+        by filtering coefficients in order of magnitude
+        (from least influential to most)
+        """
+        self.losses = list()
         ω,T=s.shape
-        z = self.F(s)
-        nonzero_indexes = argwhere(z.reshape(ω*T))
-        indexes_in_magnitude_order = list(filter(
-            lambda index:index in nonzero_indexes,
-            abs(z).reshape(ω*T).argsort()[::-1]
-        ))
-        ɸ_best = list(self.ɸ(ω=ω,T=T,l=0))
-        loss = ω*T
-        self.losses = [loss]
-        compression_length = 1
-        while loss: 
-            ɸ_best,loss = self.leastLossy(
-                s=s,
-                z=z,
-                ɸ_L=list(self.expand_compression_length(
-                    ɸ_best_smaller=ɸ_best[:beam_width],
-                    ω=ω,
-                    T=T,
-                    next_indexes_flattened=indexes_in_magnitude_order,
-                    expansion=expansion
-                ))
-            )
-            if verbose:
-                print(f"l(𝝋(z))={compression_length}, candidates={len(ɸ_best)}, loss={loss}\n") 
-            self.losses.append(loss)
-            compression_length += 1
-        return list(ɸ_best)[0]
+        z_hat = self.F(s).reshape(ω*T)
+        m = zeros(ω*T)
 
-    def leastLossy(self, s:S, z:C, ɸ_L:List[𝝋]) -> Tuple[List[𝝋],int]:
-        losses = list(map(
-            lambda 𝝋_prime:self.ɛ(
-                s=s,
-                s_hat=self.inverse_C(z_hat=𝝋_prime(z))
-            ),
-            ɸ_L
-        ))
-        min_loss = min(losses)
-        indexes_min_loss = (index for index,loss in enumerate(losses) if loss==min_loss)
-        return list(set(map(lambda index:ɸ_L[index],indexes_min_loss))),min_loss
+        for index_larger_coefficient in abs(z_hat.real).argsort():
+            z_hat[index_larger_coefficient]=0
+            loss = self.ɛ(
+                s=s, 
+                s_hat=self.inverse_C(z_hat=z_hat.reshape((ω,T)))
+            ) 
+            self.losses.append(loss)
+            if not loss:
+                m = zeros(ω*T)
+                m[flatnonzero(z_hat)]=1
+
+        return 𝝋(mask = m.reshape((ω,T)))
 
     @staticmethod
     def CR(z:C, z_hat:C) -> float:
@@ -104,7 +79,7 @@ class LosslessFourierCompression:
     @staticmethod
     def ɛ(s:S,s_hat:S) -> float:
         """reconstruction loss"""
-        return abs(array(s) - array(s_hat)).sum()
+        return abs(s - s_hat).sum()
 
     @staticmethod
     def l(z:C) -> int:
@@ -127,79 +102,10 @@ class LosslessFourierCompression:
         return array(r>=θ,dtype=int)
 
     @staticmethod
-    def ɸ(ω:int,T:int, l:int) -> Generator[𝝋,None,None]:
-        """filters of compression length l"""
-        m = zeros(ω*T)
-        m[:l] = 1 
-        for m_prime in multiset_permutations(m):
-            yield 𝝋(mask=array(m_prime).reshape((ω,T)))
-
-    @staticmethod
-    def expand_compression_length(
-        ɸ_best_smaller:List[𝝋],
-        next_indexes_flattened:List[int],
-        ω:int,
-        T:int,
-        expansion:int
-    ) -> Generator[𝝋,None,None]:
-        for 𝝋_smaller in ɸ_best_smaller:
-            mask_flattened = deepcopy(𝝋_smaller.m.reshape(ω*T))
-            nonzero_indexes = argwhere(mask_flattened)
-            next_indexes_flattened = list(filter(
-                lambda index:index not in nonzero_indexes,
-                next_indexes_flattened
-            ))
-            for 𝝋_expanded in LosslessFourierCompression.ɸ_expanded_by_index(
-                mask_flattened=mask_flattened,
-                next_indexes_flattened=next_indexes_flattened,
-                ω=ω,T=T,expansion=expansion
-            ):
-                yield 𝝋_expanded
-
-    @staticmethod
-    def ɸ_expanded_by_index(
-        mask_flattened:List[int],
-        next_indexes_flattened:List[int],
-        ω:int,
-        T:int,
-        expansion:int
-    ) -> Generator[𝝋,None,None]:
-        for index in next_indexes_flattened[:expansion]:
-            m = deepcopy(mask_flattened)
-            m[index] = 1
-            yield 𝝋(mask=m.reshape((ω,T)))
-
-
-
-
-
-
-# def ɸ_optimised(ω:int,T:int, l:int, nonzero_indexes:List[int]) -> Generator[List[List[int]],None,None]:
-#     """filters of compression length l"""
-#     m_nonzero = zeros(len(nonzero_indexes),dtype=int)
-#     m_nonzero[:l] = 1 
-#     for m_nonzero_ in multiset_permutations(m_nonzero):
-#         nonzero_indexes_ = array(m_nonzero_).nonzero()[0]
-#         indexes = nonzero_indexes[nonzero_indexes_]
-#         m_ = zeros(ω*T,dtype=int)
-#         m_[indexes] = 1
-#         m = array(m_).reshape((ω,T))
-#         yield 𝝋(mask=m)
-
-# def findOptimalFilter(s:List[List[int]]) -> 𝝋:
-#     """greedy search for optimal lossless filter"""
-#     ω,T=s.shape
-#     size = ω*T
-#     z = F(s)
-#     total_nonzero_indexes = z.reshape(size).nonzero()[0]
-#     for length in range(len(total_nonzero_indexes)+1):
-#         for 𝝋_x in ɸ_optimised(ω=ω,T=T,l=length,nonzero_indexes=total_nonzero_indexes):
-#             if ɛ(s=s,s_hat=inverse_C(𝝋_x(z)))==0:
-#                 print(𝝋_x)
-#                 return 𝝋_x
-
-
-# #============================
-
-
+    def display(s:S,z:C,z_hat:C,r_hat:R,s_hat:S,losses:List[float]) -> None:
+        #plot(losses)
+        #xlabel("Compression Length l(z_hat)")
+        #ylabel("Loss ɛ(s,s_hat)")
+        #show()
+        raise NotImplementedError
 
